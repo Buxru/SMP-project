2d3bb48c08bb4e05a72b997a6863549d30605240
Buxru <nikita.pokora.2004@gmail.com>
2023-05-01 00:30:11 +0300

    добавлен раздел главных и последних новостей

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import Header from "./Components/Header/header";
 import Underheader from "./Components/Underheader/Underheader";
+import News_feed from "./Components/News_feed/News_feed";
+
 
 function App() {
   return (
     <div className="App">
       <Header/>
         <Underheader/>
+        <News_feed/>
     </div>
   );
 }
--- src/Components/News_feed/News_feed.jsx
+++ src/Components/News_feed/News_feed.jsx
@@ -1,61 +1,98 @@
 import React from 'react';
-import Main_news_img from './../../assets/News_feed/main_news.svg'
-import Last_news_img1 from './../../assets/News_feed/last_news_1.svg'
-import Last_news_img2 from './../../assets/News_feed/last_news_2.svg'
-import Last_news_img3 from './../../assets/News_feed/last_news_3.svg'
-import Last_news_img4 from './../../assets/News_feed/last_news_4.svg'
+import './News_feed.css';
+import {Image, Title} from "@mui/icons-material";
 
 const News_feed = () => {
     return (
 
         <div className="news_feed">
             <div className="container">
-
+                <Main_news/>
+                <Last_news/>
             </div>
+
         </div>
     );
 };
 const Main_news = () => {
+    const Main_news = {
+        image: require('./../../assets/News_feed/main_news.svg').default,
+        genre: 'some genre',
+        hotTitle: 'some hot title',
+        title: 'title',
+        description: this,
+        time: this
+    }
     return (
+        <div className="main_news">
+            <div className="main_news_img">
+                <img src={Main_news.image} alt="main_news_img"/>
+            </div>
+            <div className="genre">{Main_news.genre}</div>
+            <div className="news_name">
+                <span>{Main_news.hotTitle}</span> {Main_news.title}
+            </div>
+            <div className="news_description">
+                {Main_news.description}
+            </div>
+            <div>
+                <h6>{Main_news.time}</h6>
+            </div>
 
+        </div>
     );
 }
 const Last_news = () => {
     const news_last = [
         {
-            Title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            Description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad aliquid assumenda atque aut autem beatae culpa dignissimos doloribus est eum eveniet hic ipsum mollitia nesciunt nulla officiis possimus provident quasi quod ratione recusandae temporibus, tenetur unde vel voluptas voluptatibus.',
-            Image: 'this image',
-            Time: this
-        },
-        {
-            Title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            Description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad aliquid assumenda atque aut autem beatae culpa dignissimos doloribus est eum eveniet hic ipsum mollitia nesciunt nulla officiis possimus provident quasi quod ratione recusandae temporibus, tenetur unde vel voluptas voluptatibus.',
-            Image: 'this image',
-            Time: this
-        },
-        {
-            Title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            Description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad aliquid assumenda atque aut autem beatae culpa dignissimos doloribus est eum eveniet hic ipsum mollitia nesciunt nulla officiis possimus provident quasi quod ratione recusandae temporibus, tenetur unde vel voluptas voluptatibus.',
-            Image: 'this image',
-            Time: this
-        },
-        {
-            Title: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
-            Description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad aliquid assumenda atque aut autem beatae culpa dignissimos doloribus est eum eveniet hic ipsum mollitia nesciunt nulla officiis possimus provident quasi quod ratione recusandae temporibus, tenetur unde vel voluptas voluptatibus.',
-            Image: 'this image',
-            Time: this
+            HotTitle: 'Lorem ipsum',
+            Title: 'dolor sit amet, consectetur adipisicing elit.',
+            Image: require('./../../assets/News_feed/main_news.svg').default,
+            Time: '3 minutes'
+        }, {
+            HotTitle: 'Lorem ipsum',
+            Title: 'dolor sit amet, consectetur adipisicing elit.',
+            Image: require('./../../assets/News_feed/last_news_2.svg',).default,
+            Time: '3 minutes'
+        }, {
+            HotTitle: 'Lorem ipsum',
+            Title: 'dolor sit amet, consectetur adipisicing elit.',
+            Image: require('./../../assets/News_feed/last_news_3.svg',).default,
+            Time: '3 minutes'
+        }, {
+            HotTitle: 'Lorem ipsum',
+            Title: 'dolor sit amet, consectetur adipisicing elit.',
+            Image: require('./../../assets/News_feed/last_news_4.svg',).default,
+            Time: '3 minutes'
         }
+
     ];
     return (
-        <div className="last_news">
-            {
-                news_last.map((element, index) =>(
-                <div key={index}>
-                {element.text}
-            </div>
-                ))}
-        </div>
+        <>
+            {news_last.map((element, index) => (
+                <div className="last_news" key={index}>
+                    <div className="news_name">
+                        <span>{element.HotTitle}</span> {element.Title}
+                        <br />
+                        <br />
+                        <div>
+                            <h6>{element.Time}</h6>
+                        </div>
+
+
+                    </div>
+                    <div className="last_news_img">
+                    <img src={element.Image} alt="last_news_1" />
+                </div>
+                    {index < 3 && (
+                        <div className="line" style={{ position: "relative", marginTop: "20px"}}></div>
+                    )}
+                </div>
+            ))}
+        </>
     );
+
+
+
 }
 export default News_feed;
