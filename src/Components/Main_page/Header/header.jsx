8e1f30fa58205f177f4c41bf37ffd437a7137d2e
Евгений Нурешный <85797699+PylayInFire@users.noreply.github.com>
2023-05-05 16:05:49 +0300

    Пофиксил состояния и передачу функций

--- src/Components/Main_page/Header/header.jsx
+++ src/Components/Main_page/Header/header.jsx
@@ -2,7 +2,6 @@ import React, {useState} from 'react';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import SearchIcon from '@mui/icons-material/Search';
 import MenuOpenIcon from '@mui/icons-material/MenuOpen';
-import Logo from '../../../assets/Header/logo.svg';
 import './Header.css';
 import { yellow } from '@mui/material/colors';
 import {Button, ThemeProvider} from "@mui/material";
@@ -21,7 +20,8 @@ import Settings from '@mui/icons-material/Settings';
 import Logout from '@mui/icons-material/Logout';
 import PersonAddIcon from '@mui/icons-material/PersonAdd';
 import LoginIcon from '@mui/icons-material/Login';
-
+import Login from "../../Login/login";
+import Logo from '../../../assets/Header/logo.svg';
 
 
 
@@ -44,32 +44,32 @@ const Header = () => {
     };
     return (
         <>
-        <ThemeProvider theme={theme}>
-        <div className="header">
-                <div className="header_container">
-                    <div className="menu_logo_search_icon">
-                        <Tooltip title="Інтерактивне меню">
-                        <Button className = "menu_icon" color="clicked_icon">
-                            <MenuOpenIcon sx={{color: yellow[600], width: 40, height: 40}}/>
-                        </Button>
-                        </Tooltip>
-                        <Tooltip title="Пошук новини">
-                        <Button className="search_icon" color="clicked_icon" onClick = {handleClick}>
-                            <SearchIcon sx={{color: yellow[600], width: 40, height: 40}}/>
-                        </Button>
-                        </Tooltip>
+            <ThemeProvider theme={theme}>
+                <div className="header">
+                    <div className="header_container">
+                        <div className="menu_logo_search_icon">
+                            <Tooltip title="Інтерактивне меню">
+                                <Button className = "menu_icon" color="clicked_icon">
+                                    <MenuOpenIcon sx={{color: yellow[600], width: 40, height: 40}}/>
+                                </Button>
+                            </Tooltip>
+                            <Tooltip title="Пошук новини">
+                                <Button className="search_icon" color="clicked_icon" onClick = {handleClick}>
+                                    <SearchIcon sx={{color: yellow[600], width: 40, height: 40}}/>
+                                </Button>
+                            </Tooltip>
+
+                        </div>
+                        <div className="logo_icon">
+                            <img src={Logo} alt="Logo_icon"/>
+                        </div>
+                        <div className="profile_icon_div">
+                            <Profile/>
+                        </div>
 
                     </div>
-                    <div className="logo_icon">
-                        <img src={Logo} alt="Logo_icon"/>
-                    </div>
-                    <div className="profile_icon_div">
-                        <Profile/>
-                    </div>
-
-            </div>
-        </div>
-        </ThemeProvider>
+                </div>
+            </ThemeProvider>
             <Search clicked = {isButtonClicked}/>
         </>
     );
@@ -98,6 +98,7 @@ const Profile = () => {
         avatar: 'https://s6.stc.all.kpcdn.net/afisha/msk/wp-content/uploads/sites/5/2023/01/kadr-iz-filma-avatar-put-vody.jpg'
     }
     const [anchorEl, setAnchorEl] = useState(false);
+    const [isLoginOpen, setLogin] = useState(false);
     const open = anchorEl;
     const handleClick = (event) => {
         event.preventDefault();
@@ -106,77 +107,90 @@ const Profile = () => {
     const handleClose = () => {
         setAnchorEl(false);
     };
+    const handleLoginOpen = () => {
+        setLogin(true);
+    }
+    const handleLoginClose = () => {
+        setLogin(false);
+        console.log(isLoginOpen);
+    }
+    const [checked, setChecked] = useState(false);
+    const handleCheckboxChange = (event) => {
+        setChecked(event.target.checked);
+    };
+
     const logged_in = false;
     const profile_menu_color = "444445";
     return(    <>
-        <Box sx={{ display: 'flex', alignItems: 'center',    textAlign: 'center' }}>
-            <Tooltip title="Профіль">
-                <Button
-                    sx={{color: yellow[600], width: 70, height: 70}}
-                    className="profile_icon"
-                    onClick={handleClick}
-                    size="small"
-                    aria-controls={open ? 'account-menu' : undefined}
-                    aria-haspopup="true"
-                    aria-expanded={open ? 'true' : undefined}
-                >
-                    <Avatar sx={{ width: 50, height: 50 }} src={user_info.avatar}></Avatar>
-                </Button>
-            </Tooltip>
-        </Box>
+            <Login open={isLoginOpen} closeWindow={handleLoginClose} />
+            <Box sx={{ display: 'flex', alignItems: 'center',    textAlign: 'center' }}>
+                <Tooltip title="Профіль">
+                    <Button
+                        sx={{color: yellow[600], width: 70, height: 70}}
+                        className="profile_icon"
+                        onClick={handleClick}
+                        size="small"
+                        aria-controls={open ? 'account-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                    >
+                        <Avatar sx={{ width: 50, height: 50 }} src={user_info.avatar}></Avatar>
+                    </Button>
+                </Tooltip>
+            </Box>
             {logged_in? <Menu
-                anchorEl={anchorEl}
-                id="account-menu"
-                open={open}
-                onClose={handleClose}
-                onClick={handleClose}
-                PaperProps={{
-                    elevation: 0,
-                    sx: {
-                        bgcolor: "#242526",
-                        overflow: "visible",
-                        filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                        mt: 1.5,
-                        '& .MuiAvatar-root': {
-                            width: 32,
-                            height: 32,
-                            ml: -0.5,
-                            mr: 1,
+                    anchorEl={anchorEl}
+                    id="account-menu"
+                    open={open}
+                    onClose={handleClose}
+                    onClick={handleClose}
+                    PaperProps={{
+                        elevation: 0,
+                        sx: {
+                            bgcolor: "#242526",
+                            overflow: "visible",
+                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
+                            mt: 1.5,
+                            '& .MuiAvatar-root': {
+                                width: 32,
+                                height: 32,
+                                ml: -0.5,
+                                mr: 1,
+                            },
+                            '&:before': {
+                                content: '""',
+                                display: 'block',
+                                position: 'absolute',
+                                top: 0,
+                                right: 14,
+                                width: 10,
+                                height: 10,
+                                bgcolor: '#999999',
+                                transform: 'translateY(-50%) rotate(45deg)',
+                                zIndex: 0,
+                            },
                         },
-                        '&:before': {
-                            content: '""',
-                            display: 'block',
-                            position: 'absolute',
-                            top: 0,
-                            right: 14,
-                            width: 10,
-                            height: 10,
-                            bgcolor: '#999999',
-                            transform: 'translateY(-50%) rotate(45deg)',
-                            zIndex: 0,
-                        },
-                    },
-                }}
-                transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-            >
-                <MenuItem onClick={handleClose} sx={{color: "#F2F2F2"}}>
-                    <Avatar src={user_info.avatar}/> Профіль
-                </MenuItem>
-                <Divider />
-                <MenuItem onClick={handleClose} sx={{color: "#F2F2F2"}}>
-                    <ListItemIcon >
-                        <Settings fontSize="small" sx={{color: "#FFC700"}}/>
-                    </ListItemIcon>
-                    Налаштування
-                </MenuItem>
-                <MenuItem onClick={handleClose} sx={{color: "#F2F2F2"}}>
-                    <ListItemIcon>
-                        <Logout fontSize="small" sx={{color: "#FFC700"}}/>
-                    </ListItemIcon>
-                    Вийти з аккаунту
-                </MenuItem>
-            </Menu>
+                    }}
+                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
+                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
+                >
+                    <MenuItem onClick={handleClick} sx={{color: "#F2F2F2"}}>
+                        <Avatar src={user_info.avatar}/> Профіль
+                    </MenuItem>
+                    <Divider />
+                    <MenuItem onClick={handleClose} sx={{color: "#F2F2F2"}}>
+                        <ListItemIcon >
+                            <Settings fontSize="small" sx={{color: "#FFC700"}}/>
+                        </ListItemIcon>
+                        Налаштування
+                    </MenuItem>
+                    <MenuItem onClick={handleClose} sx={{color: "#F2F2F2"}}>
+                        <ListItemIcon>
+                            <Logout fontSize="small" sx={{color: "#FFC700"}}/>
+                        </ListItemIcon>
+                        Вийти з аккаунту
+                    </MenuItem>
+                </Menu>
 
 
 
@@ -216,13 +230,13 @@ const Profile = () => {
                     transformOrigin={{ horizontal: 'right', vertical: 'top' }}
                     anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
                 >
-                    <MenuItem onClick={handleClose} sx={{color: "#F2F2F2"}}>
+                    <MenuItem onClick={handleLoginOpen} sx={{color: "#F2F2F2"}}>
                         <ListItemIcon >
                             <LoginIcon fontSize="small" sx={{color: "#FFC700"}}/>
                         </ListItemIcon>
                         Увійти
                     </MenuItem>
-                    <MenuItem onClick={handleClose} sx={{color: "#F2F2F2"}}>
+                    <MenuItem sx={{color: "#F2F2F2"}}>
                         <ListItemIcon>
                             <PersonAddIcon fontSize="small" sx={{color: "#FFC700"}}/>
                         </ListItemIcon>
@@ -231,7 +245,7 @@ const Profile = () => {
                 </Menu>
             }
 
-    </>
+        </>
     );
 }
 
