b4499896f2d8c568745954b539592d38db5451f8
Евгений Нурешный <85797699+PylayInFire@users.noreply.github.com>
2023-05-05 16:06:44 +0300

    Update Login.jsx

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,94 +1,78 @@
-import React from 'react';
-import {Avatar, Menu} from "@mui/material";
-import IconButton from "@mui/material/IconButton";
-import Tooltip from "@mui/material/Tooltip";
-import Typography from "@mui/material/Typography";
-import Box from "@mui/material/Box";
+import React, {useState} from 'react';
+import './login.css'
+import Logo from '../../assets/Header/logo.svg'
+import {Button, Checkbox, ThemeProvider} from "@mui/material";
+import {yellow} from "@mui/material/colors";
+import {grey} from "@mui/material/colors";
+import CloseIcon from '@mui/icons-material/Close';
+import {createTheme} from "@mui/material/styles";
 
-const Login = () => {
+const theme = createTheme({
+    status: {
+        danger: '#e53e3e',
+    },
+    palette: {
+        clicked_icon: {
+            main: '#fbbe00',
+            darker: '#fbbe00',
+        }
+    }
+});
+const Login = (props) => {
+
+    const [checked, setChecked] = useState(false);
+    const handleCheckboxChange = (event) => {
+        setChecked(event.target.checked);
+    };
+
     return (
-        <div>
-            <React.Fragment>
-                <Box sx={{ display: 'flex', alignItems: 'center', textAlign: 'center' }}>
-                    <Typography sx={{ minWidth: 100 }}>Contact</Typography>
-                    <Typography sx={{ minWidth: 100 }}>Profile</Typography>
-                    <Tooltip title="Account settings">
-                        <IconButton
-                            onClick={handleClick}
-                            size="small"
-                            sx={{ ml: 2 }}
-                            aria-controls={open ? 'account-menu' : undefined}
-                            aria-haspopup="true"
-                            aria-expanded={open ? 'true' : undefined}
-                        >
-                            <Avatar sx={{ width: 32, height: 32 }} >M</Avatar>
-                        </IconButton>
-                    </Tooltip>
-                </Box>
-                <Menu
-                    anchorEl={anchorEl}
-                    id="account-menu"
-                    open={open}
-                    onClose={handleClose}
-                    onClick={handleClose}
-                    PaperProps={{
-                        elevation: 0,
-                        sx: {
-                            overflow: 'visible',
-                            filter: 'drop-shadow(0px 2px 8px rgba(0,0,0,0.32))',
-                            mt: 1.5,
-                            '& .MuiAvatar-root': {
-                                width: 32,
-                                height: 32,
-                                ml: -0.5,
-                                mr: 1,
-                            },
-                            '&:before': {
-                                content: '""',
-                                display: 'block',
-                                position: 'absolute',
-                                top: 0,
-                                right: 14,
-                                width: 10,
-                                height: 10,
-                                bgcolor: 'background.paper',
-                                transform: 'translateY(-50%) rotate(45deg)',
-                                zIndex: 0,
-                            },
-                        },
-                    }}
-                    transformOrigin={{ horizontal: 'right', vertical: 'top' }}
-                    anchorOrigin={{ horizontal: 'right', vertical: 'bottom' }}
-                >
-                    <MenuItem onClick={handleClose}>
-                        <Avatar /> Profile
-                    </MenuItem>
-                    <MenuItem onClick={handleClose}>
-                        <Avatar /> My account
-                    </MenuItem>
-                    <Divider />
-                    <MenuItem onClick={handleClose}>
-                        <ListItemIcon>
-                            <PersonAdd fontSize="small" />
-                        </ListItemIcon>
-                        Add another account
-                    </MenuItem>
-                    <MenuItem onClick={handleClose}>
-                        <ListItemIcon>
-                            <Settings fontSize="small" />
-                        </ListItemIcon>
-                        Settings
-                    </MenuItem>
-                    <MenuItem onClick={handleClose}>
-                        <ListItemIcon>
-                            <Logout fontSize="small" />
-                        </ListItemIcon>
-                        Logout
-                    </MenuItem>
-                </Menu>
-            </React.Fragment>
-            );
-        </div>
+        <>
+            {props.open ? <ThemeProvider theme={theme}>
+                <div className="blur" onClick={props.closeWindow}></div>
+                <div className="login">
+                    <div className="login_area">
+                        <div className="logo_login">
+                            <img src={Logo} alt="Logo_icon"/>
+                            <Button className="close_btn" color="clicked_icon" onClick={props.closeWindow}
+                                    sx={{color: yellow[600], width: 30, height: 50}}>
+                                <CloseIcon className="close_icon" sx={{color: yellow[600], width: 30, height: 30}}/>
+                            </Button>
+                        </div>
+                        <div className="tip">
+                            Вхід до аккаунту
+                        </div>
+                        <div className="e-mail">
+                            <input style={{fontSize: '16px'}} placeholder="Ім’я користувача або e-mail"/>
+                        </div>
+                        <div className="password">
+                            <input type="password" style={{fontSize: '16px'}} placeholder="Пароль"/>
+                        </div>
+                        <div className="acception">
+                            <Checkbox
+                                color="clicked_icon"
+                                checked={checked}
+                                onChange={handleCheckboxChange}
+                                label="Запам'ятати мене"
+                                sx={{'& .MuiSvgIcon-root': {fontSize: 28}, color: yellow[600]}}
+                            />
+                            Запам'ятати мене
+                            <div className="login_div">
+                                <Button className="login_btn"
+                                        sx={{color: grey[100], bgcolor: yellow[700]}}>
+                                    Увійти
+                                </Button>
+                            </div>
+                        </div>
+                        <div className="statement">
+                            <span>Не маєте аккаунту? <a href="#">Зареєструйтесь</a></span>
+                            <br/>
+                            <span><a href="#">Забули логін або пароль?</a></span>
+                        </div>
+                    </div>
+                </div>
+            </ThemeProvider>
+            : null}
+        </>
     );
 };
 
